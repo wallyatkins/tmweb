97eb17909a519d27d9c19dd906ecfb630065a2db
Wally Atkins <wallyatkins@gmail.com>
2017-09-01 16:42:25 -0400

    retrieving data successfully now

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,85 +1,177 @@
-import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Map.Entry;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.Entity;
-import javax.ws.rs.core.Cookie;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.NewCookie;
 import javax.ws.rs.core.Response;
+
+import org.glassfish.jersey.logging.LoggingFeature;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import io.javalin.Javalin;
 
 public class Main {
 
-	private static final String LOGIN_URI = "https://tmweb.troopmaster.com/Login/Login";
-	private static final String SCOUTS_URI = "https://tmweb.troopmaster.com/ScoutManagement/index";
-	private static Client client = ClientBuilder.newClient();
-	
-	private static ArrayList<Cookie> cookies = new ArrayList<Cookie>();
-	
-	private static String cookie = "";
-		
-    // Main function that starts the web app
-    public static void main(String[] args) {
-        Javalin app = Javalin.create()
-            .port(getHerokuAssignedPort())
-            .enableStaticFiles("/public")
-            .get("/login", (req, res) -> res.body(doGetLogin()))
-            .get("/scouts", (req, res) -> res.body(doGetScouts()));
-            //.post("/", (req, res) -> res.body(addPlaceName(req.bodyAsClass(Place.class))))
-            //.delete("/*", (req, res) -> res.body(removePlaceName(req.path().substring(1)) ? "deleted" : "not found"));
-    }
+	private static int PORT = 7000;
 
-    // Helper function to get the port Heroku wants us to use
-    private static int getHerokuAssignedPort() {
-        ProcessBuilder processBuilder = new ProcessBuilder();
-        if (processBuilder.environment().get("PORT") != null) {
-            return Integer.parseInt(processBuilder.environment().get("PORT"));
-        }
-        return 7000;
-    }
-    
-    private static String doGetLogin() {
-    	Response response = getLogin(System.getenv("TM_UNIT"), System.getenv("TM_USER"), System.getenv("TM_PASS"));
-    	
-    	String output = "";
-		output += "Status: " + response.getStatus();
-		for (Entry<String, NewCookie> entry  : response.getCookies().entrySet()) {
-			output += entry.getKey() + "=" + entry.getValue();
-			cookies.add(entry.getValue());
+	private static String TM_UNIT = "";
+	private static String TM_USER = "";
+	private static String TM_PASS = "";
+
+	private static String LOGIN_URI = "https://tmweb.troopmaster.com/Login/Login";
+	private static String SCOUTS_URI = "https://tmweb.troopmaster.com/ScoutManagement/index";
+	private static String ADULTS_URI = "https://tmweb.troopmaster.com/AdultManagement/index";
+
+	private static Client client = null;
+
+	private static Map<String, String> cookies = new HashMap<String, String>();
+	
+	private static Map<String, Scout> scouts = new HashMap<String, Scout>();
+	private static Map<String, Adult> adults = new HashMap<String, Adult>();
+
+	public static void main(String[] args) {
+		initLocalEnvironment();
+		//initHerokuEnvironment();
+
+		Logger logger = Logger.getLogger(Main.class.getName());
+		LoggingFeature feature = new LoggingFeature(logger, Level.INFO, null, null);
+		client = ClientBuilder.newBuilder().register(feature).build();
+
+		// startJavalin();
+		
+		performLogin();
+		parseScouts();
+		parseAdults();
+		
+		printScouts();
+		printLeaders();
+	}
+
+	private static void initHerokuEnvironment() {
+		ProcessBuilder processBuilder = new ProcessBuilder();
+		if (processBuilder.environment().get("PORT") != null) {
+			PORT = Integer.parseInt(processBuilder.environment().get("PORT"));
 		}
-		
-		return output;
-    }
-    
-    // I was forgetting to add the TroopMasterWebSiteID cookie - that's why I wasn't getting back the desired data
-    // Now I need to process the HTML that comes back with JSoup or something ...
-    private static String doGetScouts() {
-    	Response response = getScouts();
-    	
-    	String output = response.readEntity(String.class);
-		
-		return output;
-    }
-    
-	public static Response getLogin(String unit, String user, String pass) {
+		TM_UNIT = System.getenv("TM_UNIT");
+		TM_USER = System.getenv("TM_USER");
+		TM_PASS = System.getenv("TM_PASS");
+		cookies.put("TroopMasterWebSiteID", TM_UNIT);
+	}
+
+	private static void initLocalEnvironment() {
+		TM_UNIT = "202574";
+		TM_USER = "";
+		TM_PASS = "";
+		cookies.put("TroopMasterWebSiteID", TM_UNIT);
+	}
+
+	private static void startJavalin() {
+		Javalin app = Javalin.create()
+				.port(PORT)
+				.enableStaticFiles("/public");
+				//.get("/login", (req, res) -> res.body("Status: "  + performLogin()))
+				//.get("/scouts", (req, res) -> res.body(parseScouts()));
+	}
+
+	private static int performLogin() {
+		Response response = login(TM_UNIT, TM_USER, TM_PASS);
+
+		for (Entry<String, NewCookie> entry : response.getCookies().entrySet()) {
+			cookies.put(entry.getValue().getName(), entry.getValue().getValue());
+		}
+
+		return response.getStatus();
+	}
+
+	private static void parseScouts() {
+		Response response = scouts();
+		Document html = Jsoup.parse(response.readEntity(String.class));
+		Elements rows = html.select("#grid tbody tr");
+		for (Element tr : rows) {
+			Scout scout = new Scout();
+			String[] name = tr.child(1).text().split(", ");
+			scout.setFirstName(name[1]);
+			scout.setLastName(name[0]);
+			scout.setPatrol(tr.child(2).text());
+			scout.setRank(tr.child(3).text());
+			scout.setPosition(tr.child(4).text());
+			scout.setId(tr.child(5).text());
+			scouts.put(scout.getId(), scout);
+		}
+	}
+
+	private static void parseAdults() {
+		Response response = adults();
+		Document html = Jsoup.parse(response.readEntity(String.class));
+		Elements rows = html.select("#grid tbody tr");
+		for (Element tr : rows) {
+			Adult adult = new Adult();
+			String[] name = tr.child(1).text().split(", ");
+			adult.setFirstName(name[1]);
+			adult.setLastName(name[0]);
+			adult.setLeader(tr.child(2).text().equals("Yes"));
+			adult.setTrained(tr.child(3).text().equals("Yes"));
+			adult.setPosition(tr.child(4).text());
+			adult.setId(tr.child(5).text());
+			adults.put(adult.getId(), adult);
+		}
+	}
+
+	public static Response login(String unit, String user, String pass) {
 		return client
 				.target(LOGIN_URI)
 				.request(MediaType.APPLICATION_JSON)
-				.header("Cookie", "TroopMasterWebSiteID=" + unit + ";")
+				.header("Cookie", getCookies())
 				.post(Entity.entity("{\"UserID\":\"" + user + "\",\"Password\":\"" + pass + "\"}", MediaType.APPLICATION_JSON));
 	}
-	
-	public static Response getScouts() {
-		System.out.println(cookie);
+
+	public static Response scouts() {
 		return client
 				.target(SCOUTS_URI)
 				.request(MediaType.TEXT_HTML, MediaType.APPLICATION_XHTML_XML, MediaType.APPLICATION_XML)
-				.header("Referrer", "https://tmweb.troopmaster.com/ScoutManagement/index")
-				.header("Cookie", cookie)
+				.header("Cookie", getCookies())
 				.get();
 	}
+	
+	public static Response adults() {
+		return client
+				.target(ADULTS_URI)
+				.request(MediaType.TEXT_HTML, MediaType.APPLICATION_XHTML_XML, MediaType.APPLICATION_XML)
+				.header("Cookie", getCookies())
+				.get();		
+	}
+	
+	public static String getCookies() {
+		String output = "";
+		for (String key : cookies.keySet()) {
+			output += key + "=" + cookies.get(key) + ";";
+		}
+		return output;
+	}
+	
+	public static void printScouts() {
+		for (String key : scouts.keySet()) {
+			Scout scout = scouts.get(key);
+			System.out.println(scout.getFirstName() + " " + scout.getLastName() + " [" + scout.getRank() + "] - " + scout.getPatrol());
+		}
+	}
 
+	public static void printLeaders() {
+		for (String key : adults.keySet()) {
+			Adult adult = adults.get(key);
+			if (adult.isLeader()) {
+				System.out.println(adult.getFirstName() + " " + adult.getLastName() + " [" + adult.getPosition() + "]");
+			}
+		}
+	}
+	
 }
