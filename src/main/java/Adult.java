e23dc35452c43fae657c23ba27a0b260c5198125
Wally Atkins <wallyatkins@gmail.com>
2017-09-01 16:43:50 -0400

    forgot to add the pojos

---
+++ src/main/java/Adult.java
@@ -0,0 +1,48 @@
+
+public class Adult {
+
+	private String firstName;
+	private String lastName;
+	private boolean leader;
+	private boolean trained;
+	private String position;
+	private String id;
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public boolean isLeader() {
+		return leader;
+	}
+	public void setLeader(boolean leader) {
+		this.leader = leader;
+	}
+	public boolean isTrained() {
+		return trained;
+	}
+	public void setTrained(boolean trained) {
+		this.trained = trained;
+	}
+	public String getPosition() {
+		return position;
+	}
+	public void setPosition(String position) {
+		this.position = position;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+}
---
+++ src/main/java/Scout.java
@@ -0,0 +1,48 @@
+
+public class Scout {
+
+	private String firstName;
+	private String lastName;
+	private String patrol;
+	private String rank;
+	private String position;
+	private String id;
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getPatrol() {
+		return patrol;
+	}
+	public void setPatrol(String patrol) {
+		this.patrol = patrol;
+	}
+	public String getRank() {
+		return rank;
+	}
+	public void setRank(String rank) {
+		this.rank = rank;
+	}
+	public String getPosition() {
+		return position;
+	}
+	public void setPosition(String position) {
+		this.position = position;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+
+}
