5560208068e2a9e795f41df743f9264832e88c07
Wally Atkins <wallyatkins@gmail.com>
2017-09-08 12:26:28 -0400

    getting events from remote call added

---
+++ app/tmcal.js
@@ -0,0 +1,21 @@
+import request from 'superagent'
+
+let url = '/events'
+
+export function getEvents (callback) {
+  request
+    .get(url)
+    .end((err, resp) => {
+      if (!err) {
+        const events = []
+        JSON.parse(resp.text).items.map((event) => {
+          events.push({
+            start: event.start.date || event.start.dateTime,
+            end: event.end.date || event.end.dateTime,
+            title: event.summary
+          })
+        })
+        callback(events)
+      }
+    })
+}
